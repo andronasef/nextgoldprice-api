ba2588305b3bcc8f29386d6b174907508de9f00a
Andrew Nasef <androamazing@gmail.com>
2023-05-22 16:02:36 +0300

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+var cors = require('cors')
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use('/api', require('./config/routes'));
 
