d10828fc84cb464136f710a5f5f05b81d1f64287
Andrew Nasef <androamazing@gmail.com>
2023-05-22 19:04:21 +0300

    test enable cors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-var cors = require('cors')
-
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use('/api', require('./config/routes'));
 
